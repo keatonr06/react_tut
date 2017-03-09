b5abca117dae581e8664cf48d228b2b831cf17d3
Keaton Robinson <krobins@onvia.com>
2017-03-09 11:47:56 -0800

    added basic state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,29 @@ import Order from './Order';
 
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+    addFish(fish) {
+        //update our state
+        
+        const fishes = {...this.state.fishes};
+        //add in new fish
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        this.setState({ fishes});
+        //this.state.fishes.fish1 = fish;
+        //set state
+    }
+    
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +35,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seasfood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
